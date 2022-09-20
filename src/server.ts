05fd8aef849705fda88dc4fb83676bd46f30d76a
Higor Beleza <higorbeleza49@gmail.com>
2022-09-20 11:47:03 -0300

    final project 1.0

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,6 @@ server.use(express.static(path.join(__dirname, '../public') ));
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-    res.send('Página não encotrada :(')
+    res.render('pages/404')
 });
 server.listen(process.env.PORT);
